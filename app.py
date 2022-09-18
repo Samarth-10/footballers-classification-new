203be2a5640f45c6fc9aed4195f7d73bfe8a1b5b
Samarth-10 <101192291+Samarth-10@users.noreply.github.com>
2022-09-18 21:50:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ def get_b64_test_image_for_virat():
 def class_number_to_name(class_num):
         return __class_number_to_name[class_num]
 
-if __name__ == '__main__':
-    load_saved_artifacts()
+# if __name__ == '__main__':
+#     load_saved_artifacts()
 
     # print(classify_image(get_b64_test_image_for_virat(), None))
     # print(class_number_to_name(4))
