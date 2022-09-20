48cf7a14dea264d0b36029a65c18a03608d97967
Samarth-10 <101192291+Samarth-10@users.noreply.github.com>
2022-09-20 17:55:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify, render_template
-import util
 import joblib
 import json
 import numpy as np
@@ -9,7 +8,27 @@ from wavelet import w2d
 __class_name_to_number = {}
 __class_number_to_name = {}
 
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/classify_image', methods=['GET', 'POST'])
+def clasify_image():
+    image_data = request.form['image_data']
+    response = jsonify(classify_image(image_data))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    load_saved_artifacts()
+    app.run(debug=True)
+
+
+    
+    
 def classify_image(image_base64_data, file_path=None):
     __class_name_to_number = json.load(open("class_dictionary.json", "r"))
     __model = joblib.load(open('saved_model.pkl', 'rb'))
@@ -48,7 +67,6 @@ def load_saved_artifacts():
 
 
 def get_cv2_image_from_base64_string(b64str):
-    
     encoded_data = b64str.split(',')[1]
     nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
@@ -57,15 +75,12 @@ def get_cv2_image_from_base64_string(b64str):
 def get_cropped_image_if_2_eyes(image_path, image_base64_data):
     face_cascade = cv2.CascadeClassifier('/opencv/haarcascades/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('/opencv/haarcascades/haarcascade_eye.xml')
-
     if image_path:
         img = cv2.imread(image_path)
     else:
         img = get_cv2_image_from_base64_string(image_base64_data)
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
     cropped_faces = []
     for (x,y,w,h) in faces:
             roi_gray = gray[y:y+h, x:x+w]
@@ -81,43 +96,5 @@ def get_b64_test_image_for_virat():
 
 def class_number_to_name(class_num):
         return __class_number_to_name[class_num]
-
-# if __name__ == '__main__':
-#     load_saved_artifacts()
-
-    # print(classify_image(get_b64_test_image_for_virat(), None))
-    # print(class_number_to_name(4))
-    # print(classify_image(None,"E:\Samarth\python\Machine Learning\project 2\Server\\test_images\\federer1.jpg"))
-    # print(classify_image(None,"E:\Samarth\python\Machine Learning\project 2\Server\\test_images\\federer2.jpg"))
-    # print(classify_image(None,"E:\Samarth\python\Machine Learning\project 2\Server\\test_images\\serena1.jpg"))
-    # print(classify_image(None,"E:\Samarth\python\Machine Learning\project 2\Server\\test_images\\virat1.jpg"))
-    # print(classify_image(None,"E:\Samarth\python\Machine Learning\project 2\Server\\test_images\\virat3.jpg"))
-
-
     
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/classify_image', methods=['GET', 'POST'])
-def clasify_image():
-    image_data = request.form['image_data']
-
-    response = jsonify(classify_image(image_data))
-
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-
-
-
-
-if __name__ == "__main__":
-    print("Starting Python Flask Server For Sports Celebrity Image Classification")
-    load_saved_artifacts()
-    app.run(debug=True)
+    
